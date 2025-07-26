78c6d97710152a5a7d95c504aac1ce6be77e0a88
Benjamin Fovet <b.fovet@gmail.com>
2025-07-26 21:59:54 +0200

    feat: setup fastapi with health route
    + build Docker images

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 https://github.com/minio/mc/issues/3599
+
+
+PYTHONASYNCIODEBUG=1
+
+https://github.com/astral-sh/uv-docker-example
+
+docker run s3py -p 8000:8000
---
+++ src/s3py/main.py
@@ -0,0 +1,49 @@
+import uvicorn
+from fastapi import FastAPI, status
+
+from s3py.models import HealthCheck
+
+app = FastAPI()
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
+
+
+def main():
+    """Entrypoint to invoke when this module is invoked on the remote server."""
+    # See the official documentations on how "0.0.0.0" makes the service available on
+    # the local network - https://www.uvicorn.org/settings/#socket-binding
+    uvicorn.run(app, host="0.0.0.0", loop="uvloop", workers=1)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+To check if the API service works as expected, perform the following actions:
+    1. Run the API service by invoking this command - "python -m main".
+    2. If the service is running, open the URL "http://localhost:8000" in your browser.
+    3. With cURL, invoke this command:
+       "curl --include --request GET "http://localhost:8000/health" and you should
+       get a HTTP Status Code 200 OK message somewhere in it."
+An example Dockerfile with a healthcheck capabilities enabled is available in this gist:
+https://gist.github.com/Jarmos-san/11bf22c59d26daf0aa5223bdd50440da
+"""
